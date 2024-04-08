e597ec069bcce1150a910ef4cba1ecb8bcb9714a
MatZar01 <zski.mateusz@gmail.com>
2024-04-08 13:46:58 +0200

    multi step fusion model added

--- cfgs/default.py
+++ cfgs/default.py
@@ -1,7 +1,7 @@
 model_info = {
     'DATA_PATH': 'DS/IDA-BD/i_B',
     'DEVICE': 'cuda',
-    'EPOCHS': 60,
+    'EPOCHS': 30,
     'LR': 1e-4,
     'TRAIN_SIZE': 0.75,
     'FUSE_METHOD': 2,
@@ -9,5 +9,5 @@ model_info = {
     'BATCH_SIZE': 64,
     'OPT': 'Adam',
     'SCHEDULER': {'NAME': 'ROP', 'PATIENCE': 5, 'FACTOR': 0.8, 'STEP': 5, 'GAMMA': 0.5},
-    'MODEL_NAME': 'Init_Net'
+    'MODEL_NAME': 'Fuse_Net'
 }
--- src/models.py
+++ src/models.py
@@ -52,7 +52,7 @@ class Init_Net(nn.Module):
             nn.MaxPool2d((2, 2)),
             nn.Flatten()
         )
-        self.embedder_second = copy.deepcopy(self.embedder_first)
+        #self.embedder_second = copy.deepcopy(self.embedder_first)
 
         self.classifier = nn.Sequential(
             nn.Linear(12544, 4096),
@@ -66,7 +66,7 @@ class Init_Net(nn.Module):
 
     def forward(self, ft1: torch.Tensor, ft2: torch.Tensor) -> torch.Tensor:
         emb_1 = self.embedder_first(ft1)
-        emb_2 = self.embedder_second(ft2)
+        emb_2 = self.embedder_first(ft2)
 
         fused = fuse_fts(emb_1, emb_2, method=self.fuse_method)
 
@@ -87,39 +87,64 @@ class Init_Net_NF(Init_Net):
         return torch.sigmoid(logits)
 
 
-class Fuse_Net(nn.Module):  # turn this net into multi-stage fusion
+class Fuse_Net(nn.Module):
     def __init__(self, input_size: int, output_size: int, fuse_method: int):
         super().__init__()
         self.fuse_method = fuse_method
-        self.embedder_first = nn.Sequential(
+        # initial embedding
+        self.initial_embedder_A = nn.Sequential(
             nn.BatchNorm2d(input_size[-1]),
             nn.Conv2d(input_size[-1], 16, (3, 3), 1, 1),
             nn.Conv2d(16, 32, (3, 3), 1, 1),
             nn.Conv2d(32, 64, (3, 3), 1, 1),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(64, 128, (3, 3), 1, 1),
-            nn.Conv2d(128, 128, (3, 3), 1, 1),
-            nn.Conv2d(128, 128, (3, 3), 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(128, 128, (3, 3), 1, 1),
-            nn.Conv2d(128, 256, (3, 3), 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(256, 256, (3, 3), 1, 1),
-            nn.Conv2d(256, 256, (3, 3), 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(256, 256, (3, 3), 1, 1),
-            nn.Conv2d(256, 256, (3, 3), 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Flatten()
+            nn.MaxPool2d((2, 2))
         )
-        self.embedder_second = copy.deepcopy(self.embedder_first)
+        self.initial_embedder_B = copy.deepcopy(self.initial_embedder_A)
 
+        # 1st stage embedding
+        self.stage_1_A = nn.Sequential(
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2))
+        )
+        self.stage_1_B = copy.deepcopy(self.stage_1_A)
+
+        # 2nd stage embedding
+        self.stage_2_A = nn.Sequential(
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2))
+        )
+        self.stage_2_B = copy.deepcopy(self.stage_2_A)
+
+        # 3rd stage embedding
+        self.stage_3_A = nn.Sequential(
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2))
+        )
+        self.stage_3_B = copy.deepcopy(self.stage_3_A)
+
+        # 4th stage embedding
+        self.stage_4_A = nn.Sequential(
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.Conv2d(64, 64, (3, 3), 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2))
+        )
+        self.stage_4_B = copy.deepcopy(self.stage_4_A)
+
+        # final classifier
         self.classifier = nn.Sequential(
+            nn.Flatten(),
             nn.Linear(12544, 4096),
             nn.ReLU(),
             nn.Linear(4096, 512),
@@ -130,10 +155,34 @@ class Fuse_Net(nn.Module):  # turn this net into multi-stage fusion
         )
 
     def forward(self, ft1: torch.Tensor, ft2: torch.Tensor) -> torch.Tensor:
-        emb_1 = self.embedder_first(ft1)
-        emb_2 = self.embedder_second(ft2)
+        # initial embedding
+        emb_A = self.initial_embedder_A(ft1)
+        emb_B = self.initial_embedder_B(ft2)
 
-        fused = fuse_fts(emb_1, emb_2, method=self.fuse_method)
+        # 1st stage
+        emb_A = self.stage_1_A(emb_A)
+        emb_B = self.stage_1_B(emb_B)
+        fused_1 = fuse_fts(emb_A, emb_B, method=self.fuse_method)
+        fused_1 = nn.functional.max_pool2d(fused_1, (8, 8))
 
-        logits = self.classifier(fused)
+        # 2nd stage
+        emb_A = self.stage_2_A(emb_A)
+        emb_B = self.stage_2_B(emb_B)
+        fused_2 = fuse_fts(emb_A, emb_B, method=self.fuse_method)
+        fused_2 = nn.functional.max_pool2d(fused_2, (4, 4))
+
+        # 3rd stage
+        emb_A = self.stage_3_A(emb_A)
+        emb_B = self.stage_3_B(emb_B)
+        fused_3 = fuse_fts(emb_A, emb_B, method=self.fuse_method)
+        fused_3 = nn.functional.max_pool2d(fused_3, (2, 2))
+
+        # 4th stage
+        emb_A = self.stage_4_A(emb_A)
+        emb_B = self.stage_4_B(emb_B)
+        fused_4 = fuse_fts(emb_A, emb_B, method=self.fuse_method)
+
+        fused_final = torch.concatenate([fused_1, fused_2, fused_3, fused_4], dim=1)
+
+        logits = self.classifier(fused_final)
         return torch.sigmoid(logits)
